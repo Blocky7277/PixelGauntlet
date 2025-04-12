314c43acd5f0f97a7ad66533116f47384cace5ef
Blocky7277 <jacob612r@gmail.com>
2025-04-12 12:17:28 -0500

    merge

---
+++ Assets/Assist.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Assist : MonoBehaviour {
+    protected float cooldown;
+    protected float duration;
+}
