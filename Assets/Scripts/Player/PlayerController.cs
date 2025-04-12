81eaf71cd077429a225ac95522469d5b5b2fa365
Blocky7277 <jacob612r@gmail.com>
2025-04-12 09:15:29 -0500

    Player controller started

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    private float str = 10;
+
+    private float spd = 10;
+
+    private float health = 100f;
+
+    [SerializeField]
+    private float movementSpeed = 20;
+
+    void Start()
+    {
+        if (!rb) rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 direction = new(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rb.velocity = direction.normalized * movementSpeed;
+    }
+
+    public void DealDamage(float dmg) {
+        health -= dmg;
+        if (health <= 0) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Destroy(gameObject);
+        StartCoroutine(DelayLoad());
+    }
+
+    IEnumerator DelayLoad() {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
