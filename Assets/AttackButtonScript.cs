1ed00a2044a7de59ef05b65746d2d53326ee897d
Tal1762 <113717843+Tal1762@users.noreply.github.com>
2025-04-12 10:22:41 -0500

    player is prefab now

---
+++ Assets/AttackButtonScript.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPressScript : MonoBehaviour
+{
+    public void doAttack(){
+        Debug.Log("amogus");
+    }
+}
---
+++ Assets/OnPressScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnPressScript : MonoBehaviour
+{
+    [SerializeField]
+    private Script bossManager;
+    public void doAttack(){
+        Debug.Log("amogus");
+    }
+}
